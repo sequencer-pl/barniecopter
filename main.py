911bacdb02b6297ffe80df3535da89f10f213268
sequencer <sequencer@o2.pl>
2021-04-23 19:33:40 +0200

    Added setup.py with console script entry point

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import logging
 from config.config import ConfigParser
 from logger.logger import logging_setup
 
-if __name__ == '__main__':
+
+def run():
     logging_setup()
     c = ConfigParser(['config/config.yaml', 'config/secrets.yaml']).read_configs()
     logging.info(f'It works! {c}')
+
+
+if __name__ == '__main__':
+    run()
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='barniecopter',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': ['barniecopter=main:run']
+    }
+)
