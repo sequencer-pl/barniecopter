411dbe4a228870dfd1daaec58a6d160495707aab
sequencer <sequencer@o2.pl>
2021-04-25 18:06:21 +0200

    Configs validation

--- config/config.py
+++ config/config.py
@@ -3,6 +3,11 @@ from mergedeep import merge
 
 
 class ConfigParser:
+    MANDATORY_KEYS = {
+        'TAAPI': ['base_url', 'exchange', 'indicator', 'symbol', 'interval', 'api_key'],
+        'CMC': ['base_url', 'endpoint', 'symbol', 'x-cmc-pro-api-key']
+    }
+
     def __init__(self, config_files):
         self.config_files = config_files
 
@@ -11,10 +16,25 @@ class ConfigParser:
         for file in self.config_files:
             cnf = self.read_config(file)
             merge(configs, configs, cnf)
-        # TODO self.validate(configs)
+        self.validate(configs)
         return configs
 
     @staticmethod
     def read_config(file):
         with open(file) as f:
             return yaml.safe_load(f)
+
+    def validate(self, configs):
+        for item, sites in configs.items():
+            if set(sites.keys()) != set(self.MANDATORY_KEYS.keys()):
+                e = f"{item} should contain {self.MANDATORY_KEYS.keys()}"
+                raise ValidationError(e)
+            for site, params in sites.items():
+                diff = set(params).symmetric_difference(set(self.MANDATORY_KEYS[site]))
+                if diff:
+                    e = f"Lack or improper key {', '.join(diff)} in {site} entry"
+                    raise ValidationError(e)
+
+
+class ValidationError(Exception):
+    pass
--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -1,9 +1,56 @@
 from unittest import TestCase, mock
 
-from config.config import ConfigParser
+from config.config import ConfigParser, ValidationError
 
 
 class ConfigTest(TestCase):
+
+    config_file = {
+        'BTC': {
+            'TAAPI': {
+                'base_url': 'https://api.taapi.io/',
+                'exchange': 'binance',
+                'indicator': 'macd',
+                'symbol': 'BTC/USDT',
+                'interval': '1h',
+            },
+            'CMC': {
+                'base_url': 'https://pro-api.coinmarketcap.com',
+                'endpoint': '/v1/cryptocurrency/quotes/latest',
+                'symbol': 'BTC',
+            }
+        }
+    }
+
+    secrets_file = {
+        'BTC': {
+            'TAAPI': {
+                'api_key': 'taapi-secret',
+            },
+            'CMC': {
+                'x-cmc-pro-api-key': 'cmc-secret',
+            }
+        }
+    }
+
+    merged_config = {
+        'BTC': {
+            'TAAPI': {
+                'base_url': 'https://api.taapi.io/',
+                'exchange': 'binance',
+                'indicator': 'macd',
+                'symbol': 'BTC/USDT',
+                'interval': '1h',
+                'api_key': 'taapi-secret',
+            },
+            'CMC': {
+                'base_url': 'https://pro-api.coinmarketcap.com',
+                'endpoint': '/v1/cryptocurrency/quotes/latest',
+                'symbol': 'BTC',
+                'x-cmc-pro-api-key': 'cmc-secret',
+            }
+        }
+    }
 
     @mock.patch('config.config.ConfigParser.read_config')
     def test_read_config_not_called_when_empty_list_passed_to_constructor(self, read_config):
@@ -19,10 +66,8 @@ class ConfigTest(TestCase):
     @mock.patch('config.config.ConfigParser.read_config')
     def test_read_configs_return_combined_values_from_yaml_files(self, read_config):
         # given
-        config_content = {'public': 'value'}
-        secret_content = {'secret': 'password'}
-        read_config.side_effect = [config_content, secret_content]
-        expected = {**config_content, **secret_content}
+        read_config.side_effect = [self.config_file, self.secrets_file]
+        expected = self.merged_config
         cp = ConfigParser(['config.yaml', 'secret.yaml'])
 
         # when
@@ -34,10 +79,8 @@ class ConfigTest(TestCase):
     @mock.patch('config.config.ConfigParser.read_config')
     def test_read_configs_override_values_from_first_file_with_keys_from_second_file(self, read_config):
         # given
-        config_content = {'public': 'value', 'secret': ''}
-        secret_content = {'secret': 'password'}
-        read_config.side_effect = [config_content, secret_content]
-        expected = {**config_content, **secret_content}
+        read_config.side_effect = [self.config_file, self.secrets_file]
+        expected = self.merged_config
         cp = ConfigParser(['config.yaml', 'secret.yaml'])
 
         # when
@@ -49,10 +92,8 @@ class ConfigTest(TestCase):
     @mock.patch('config.config.ConfigParser.read_config')
     def test_read_configs_add_nested_values_instead_of_override_it(self, read_config):
         # given
-        config_content = {'HEAD': {'public': 'value', 'secret': ''}}
-        secret_content = {'HEAD': {'secret': 'password'}}
-        read_config.side_effect = [config_content, secret_content]
-        expected = {'HEAD': {'public': 'value', 'secret': 'password'}}
+        read_config.side_effect = [self.config_file, self.secrets_file]
+        expected = self.merged_config
         cp = ConfigParser(['config.yaml', 'secret.yaml'])
 
         # when
@@ -61,7 +102,13 @@ class ConfigTest(TestCase):
         # then
         self.assertEqual(expected, configs)
 
-    # TODO
-    # def test_validate_configs_raise_config_validation_error_if_lack_of_mandatory_keys(self):
+    @mock.patch('config.config.ConfigParser.read_config')
+    def test_read_configs_raise_validation_error_if_lack_of_mandatory_keys(self, read_config):
         # given
-        # TODO
+        read_config.return_value = self.config_file
+        cp = ConfigParser(['fake.yaml'])
+
+        # then
+        with self.assertRaises(ValidationError):
+            # when
+            cp.read_configs()
